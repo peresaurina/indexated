a948e8291e4391e6bd44547f581c1276aa8f6cf4
pere-lenovo <pere.saurina@gmail.com>
2016-11-07 20:42:04 +0100

    update

--- sitemaps_xml/analitzar_doc.php
+++ sitemaps_xml/analitzar_doc.php
@@ -8,7 +8,7 @@ $arxiu_contingut = file_get_contents("http://matcarrelage.com/1_fr_0_sitemap.xml
 $xml = new SimpleXMLElement($arxiu_contingut);
 foreach ($xml->url as $url_list) {
     $url = utf8_encode($url_list->loc);
-    echo $url . "<br>";
+    //echo $url . "<br>";
 
     
     $googleUrl = new GoogleUrl();
@@ -17,11 +17,11 @@ foreach ($xml->url as $url_list) {
     //$acdcPage1 = $googleUrl->setPage(0)->search("acdc"); // acdc results page 1 (results 1-10)
     //$acdcPage2 = $googleUrl->setPage(1)->search("acdc"); // acdc results page 2 (results 11-20)
     //$url = "bascara";
-    $googleUrl->setNumberResults(5);
+    $googleUrl->setNumberResults(1);
     $simpsonPage1 = $googleUrl->setPage(0)->search($url); // simpsons results page 1 (results 1-20)
     //
     //
-    print_r($simpsonPage1);
+    //print_r($simpsonPage1);
     
     // GET NATURAL RESULTS
     $positions = $simpsonPage1->getPositions();
@@ -35,6 +35,8 @@ foreach ($xml->url as $url_list) {
         echo "<li>title : " . utf8_decode($result->getTitle()) . "</li>";
         echo "<li>website : " . $result->getWebsite() . "</li>";
         echo "<li>URL : <a href='" . $result->getUrl() . "'>" . $result->getUrl() . "</a></li>";
+        echo "<li>URL : <a href='" . $url . "'>" . $url . "</a></li>";
+        if ($url == $result->getUrl()) echo "<li>És la mateixa!!!!</li>";
         echo "</ul>";
         echo "</li>";
     }
