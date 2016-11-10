e28740429ccb62d21fc78a57f97e70c5e9dcaa90
Pere Saurina <pere.saurina@gmail.com>
2016-11-10 15:34:03 +0100

    no message

---
+++ protected/models/Indexeds.php
@@ -0,0 +1,117 @@
+<?php
+
+/**
+ * Description of Owner
+ *
+ * @peresaurina
+ */
+class Indexeds {
+
+    //<editor-fold desc="ATTRIBUTES">
+    protected $id;
+    protected $url;
+    protected $google_index;
+    protected $createdAt;
+
+    function __construct($id, $fields) {
+        if ($id != null) {
+            $this->__constructById($id);
+        }
+        if (is_array($fields) && !is_null($fields)) {
+            $this->__constructByFields($fields);
+        }
+    }
+
+    function __constructById($id) {
+        if ($id != null) {
+            $query = "SELECT * FROM `indexeds` WHERE id = '" . $id . "'";
+            $result = mysql_query($query);
+            $row = mysql_fetch_assoc($result);
+            $this->__constructByFields($row);
+        }
+    }
+
+    function __constructByFields($fields) {
+        $this->id = isset($fields["id"]) ? (int) $fields["id"] : null;
+        $this->url = isset($fields["url"]) ? $fields["url"] : null;
+        $this->google_index = isset($fields["google_index"]) ? $fields["google_index"] : 0;
+        $this->createdAt = isset($fields["createdAt"]) ? $fields["createdAt"] : null;
+        //print_r($this);
+    }
+
+    //</editor-fold>
+    //<editor-fold desc="DB METHODS">
+    /**
+     * Exists this owner in the Data base?
+     * @return boolean
+     */
+    public function insertIntoDataBase() {
+        $query = 'INSERT INTO `indexeds` SET ' .
+                ($this->url != null ? 'url = "' . $this->url . '", ' : '') .
+                ($this->google_index != null ? 'url = "' . $this->google_index . '", ' : '') .
+                'createdAt = NOW()';
+        $result = mysql_query($query);
+        $this->id = (int) mysql_insert_id();
+        print_r($query);
+        return $result;
+    }
+
+    public function existInDB($id) {
+        $query = "SELECT * FROM `indexeds` WHERE id = '" . $id . "'";
+        //echo "Claims.php : ".$query;
+        $result = mysql_query($query);
+        if (mysql_num_rows($result) > 0) {
+            return "1";
+        } else {
+            return "0";
+        }
+    }
+
+    public function existUrlDB($url) {
+        $query = "SELECT * FROM `indexeds` WHERE url = '" . $url . "'";
+        //echo "Claims.php : ".$query;
+        $result = mysql_query($query);
+        if (mysql_num_rows($result) > 0) {
+            return "1";
+        } else {
+            return "0";
+        }
+    }
+
+    //</editor-fold>
+    //<editor-fold desc="GETTERS & SETTERS">
+    function getId() {
+        return $this->id;
+    }
+
+    function getUrl() {
+        return $this->url;
+    }
+
+    function getCreatedAt() {
+        return $this->createdAt;
+    }
+
+    function setId($id) {
+        $this->id = $id;
+    }
+
+    function setUrl($url) {
+        $this->url = $url;
+    }
+
+    function setCreatedAt($createdAt) {
+        $this->createdAt = $createdAt;
+    }
+    function getGoogle_index() {
+        return $this->google_index;
+    }
+
+    function setGoogle_index($google_index) {
+        $this->google_index = $google_index;
+    }
+
+
+}
+
+?>
---
+++ sitemaps_xml/analitzar_doc.php
@@ -0,0 +1,68 @@
+<?php
+
+include ('../lib/funcions_generals.php');
+include("../google-url-master/autoload.php");
+include("../protected/models/Indexeds.php");
+include("../protected/config/main.php");
+
+$arxiu_contingut = file_get_contents("http://matcarrelage.com/1_fr_0_sitemap.xml");
+$xml = new SimpleXMLElement($arxiu_contingut);
+$i = 0;
+foreach ($xml->url as $url_list) {
+
+    $url = utf8_encode($url_list->loc);
+        
+    if (Indexeds::existUrlDB($url) == '0') {
+        
+        $googleUrl = new GoogleUrl();
+        $googleUrl->setLang('fr') // lang allows to adapt the query (tld, and google local params)
+                ->setNumberResults(10);                        // 5 results per page
+        $googleUrl->setNumberResults(1);
+        //echo "<br><br>";
+        //print_r($googleUrl);
+        
+        $simpsonPage1 = $googleUrl->setPage(0)->search($url); // simpsons results page 1 (results 1-20)
+        
+        //echo "<br><br>";
+        //print_r($simpsonPage1);
+        
+        // GET NATURAL RESULTS
+        $positions = $simpsonPage1->getPositions();
+        
+        //echo "<br><br>";
+        //print_r($positions);
+        
+        foreach ($positions as $result) {
+
+            echo "<ul>";
+            echo "<li>position : " . $result->getPosition() . "</li>";
+            echo "<li>title : " . utf8_decode($result->getTitle()) . "</li>";
+            echo "<li>website : " . $result->getWebsite() . "</li>";
+            echo "<li>URL google : <a href='" . $result->getUrl() . "'>" . $result->getUrl() . "</a></li>";
+            echo "<li>URL sitemap : <a href='" . $url . "'>" . $url . "</a></li>";
+            if ($url == $result->getUrl()) {
+                echo "<li>Es la mateixa Indexada!!!!</li>";
+                $pagina["url"] = $url;
+                $pagina["google_index"] = '1';
+                $pagina_indexada = new Indexeds(null, $pagina);
+                $pagina_indexada->insertIntoDataBase();
+            } else {
+                $pagina["url"] = $url;
+                $pagina["google_index"] = '0';
+                $pagina_indexada = new Indexeds(null, $pagina);
+                //print_r($pagina_indexada);
+                $pagina_indexada->insertIntoDataBase();
+                echo "<li>No indexada</li>";
+            }
+            echo "</ul>";
+            $i++;
+            sleep(10);
+            if ($i == 25)
+                exit();
+        }
+    }else {
+        echo "<ul><li>" . $url . " -> Ja analitzada</li></ul>";
+    }    
+    ini_set('max_execution_time', 30);
+}
+?>
