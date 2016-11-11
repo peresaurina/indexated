0c798777923c5d0789bd24d945295b07d435a88e
pere-lenovo <pere.saurina@gmail.com>
2016-11-11 22:33:25 +0100

    update

--- sitemaps_xml/analitzar_doc.php
+++ sitemaps_xml/analitzar_doc.php
@@ -18,7 +18,7 @@ foreach ($xml->url as $url_list) {
     if (Indexeds::existUrlDB($url) == '0') {
 
         $googleUrl = new GoogleUrl();
-        $googleUrl->setLang('es') // lang allows to adapt the query (tld, and google local params)
+        $googleUrl->setLang('fr') // lang allows to adapt the query (tld, and google local params)
                 ->setNumberResults(10);                        // 5 results per page
         $googleUrl->setNumberResults(1);
         $simpsonPage1 = $googleUrl->setPage(0)->search($url); // simpsons results page 1 (results 1-20)
