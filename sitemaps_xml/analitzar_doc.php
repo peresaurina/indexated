7d7e5534aa5c6e88444b35f1b4ed41935fd8a867
Pere Saurina <pere.saurina@gmail.com>
2016-11-28 12:04:41 +0100

    no message

--- sitemaps_xml/analitzar_doc.php
+++ sitemaps_xml/analitzar_doc.php
@@ -16,7 +16,7 @@ foreach ($xml->url as $url_list) {
     echo "<br>" . $i . " web : " . $url;
 
     if (Indexeds::existUrlDB($url) == '0') { //existeix a la base dades
-
+        echo "<br> pos 10";
         $googleUrl = new GoogleUrl();
         $googleUrl->setLang('fr') // lang allows to adapt the query (tld, and google local params)
                 ->setNumberResults(10);                        // 5 results per page
@@ -24,9 +24,9 @@ foreach ($xml->url as $url_list) {
         $simpsonPage1 = $googleUrl->setPage(0)->search($url); // simpsons results page 1 (results 1-20)
         // GET NATURAL RESULTS
         $positions = $simpsonPage1->getPositions();
-
+echo "<br> pos 15";
         foreach ($positions as $result) {
-
+echo "<br> pos 20";
             //echo "<ul>";
             //echo "<li>position : " . $result->getPosition() . "</li>";
             //echo "<li>title : " . utf8_decode($result->getTitle()) . "</li>";
