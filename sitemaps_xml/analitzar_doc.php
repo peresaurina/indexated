5f72e1c74e912c034fc98f7e004b33a9aac747d0
pere-lenovo <pere.saurina@gmail.com>
2016-11-28 20:59:18 +0100

    update

--- sitemaps_xml/analitzar_doc.php
+++ sitemaps_xml/analitzar_doc.php
@@ -13,53 +13,57 @@ $i = 1;
 foreach ($xml->url as $url_list) {
     $url = utf8_encode($url_list->loc);
     echo "<br>" . $i . " web : " . $url;
-    
-    $googleUrl = new GoogleUrl();
-    $googleUrl->setLang('fr') // lang allows to adapt the query (tld, and google local params)
-            ->setNumberResults(10);                        // 5 results per page
-    
-    $googleUrl->setNumberResults(10);
-    
-    $simpsonPage1 = $googleUrl->setPage(0)->search($url); // simpsons results page 1 (results 1-20)
-    
-    // GET NATURAL RESULTS
-    $positions = $simpsonPage1->getPositions();
-    
-    foreach ($positions as $result) {
-        
-        //echo "<ul>";
-        //echo "<li>position : " . $result->getPosition() . "</li>";
-        //echo "<li>title : " . utf8_decode($result->getTitle()) . "</li>";
-        //echo "<li>website : " . $result->getWebsite() . "</li>";
-        //echo "<li>URL google : <a href='" . $result->getUrl() . "'>" . $result->getUrl() . "</a></li>";
-        //echo "<li>URL sitemap : <a href='" . $url . "'>" . $url . "</a></li>";
 
         if (Indexeds::existUrlDB($url) == '0') {
-            if ($url == $result->getUrl()) {
-                echo "<br>Es la mateixa Indexada";
-                $pagina["url"] = $url;
-                $pagina["google_url1"] = '';
-                $pagina["google_index"] = '1';
-                $print_r($pagina);
-                $pagina_indexada = new Indexeds(null, $pagina);
-                $print_r($pagina_indexada);
-                $pagina_indexada->insertIntoDataBase();
-            } else {
-                $pagina["url"] = $url;
-                $pagina["google_index"] = '0';
-                $pagina["google_url1"] = $result->getUrl();
-                $pagina_indexada = new Indexeds(null, $pagina);
-                print_r($pagina_indexada);
-                $pagina_indexada->insertIntoDataBase();
-                echo "<br>No indexada</li>";
-            }
-            echo "</ul>";
-            sleep(30);
-        }
-    }
-    $i++;
-    echo "-> Ja analitzada";
+    
+                $googleUrl = new GoogleUrl();
+                $googleUrl->setLang('fr') // lang allows to adapt the query (tld, and google local params)
+                        ->setNumberResults(10);                        // 5 results per page
+                
+                $googleUrl->setNumberResults(10);
+                
+                $simpsonPage1 = $googleUrl->setPage(1)->search($url); // simpsons results page 1 (results 1-20)
+                
+                // GET NATURAL RESULTS
+                $positions = $simpsonPage1->getPositions();
+                
+                foreach ($positions as $result) {
+                    
+                    //echo "<ul>";
+                    //echo "<li>position : " . $result->getPosition() . "</li>";
+                    //echo "<li>title : " . utf8_decode($result->getTitle()) . "</li>";
+                    //echo "<li>website : " . $result->getWebsite() . "</li>";
+                    //echo "<li>URL google : <a href='" . $result->getUrl() . "'>" . $result->getUrl() . "</a></li>";
+                    //echo "<li>URL sitemap : <a href='" . $url . "'>" . $url . "</a></li>";
 
+                    if ($url == $result->getUrl()) {
+                        echo "<br>Es la mateixa Indexada";
+                        $pagina["url"] = $url;
+                        $pagina["google_url1"] = '';
+                        $pagina["google_index"] = '1';
+                        echo "<br>";
+                        $print_r($pagina);
+                        $pagina_indexada = new Indexeds(null, $pagina);
+                        echo "<br>";
+                        $print_r($pagina_indexada);
+                        $pagina_indexada->insertIntoDataBase();
+                    } else {
+                        $pagina["url"] = $url;
+                        $pagina["google_index"] = '0';
+                        $pagina["google_url1"] = $result->getUrl();
+                        $pagina_indexada = new Indexeds(null, $pagina);
+                        print_r($pagina_indexada);
+                        $pagina_indexada->insertIntoDataBase();
+                        echo "<br>No indexada</li>";
+                    }
+                    echo "</ul>";
+                    sleep(30);
+                    
+                }
+        }else{
+            $i++;
+            echo "-> Ja analitzada";
+        }
     ini_set('max_execution_time', 300);
 }
 ?>
