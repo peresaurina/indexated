5f452b7fbc1b586cb347dd9126a1f5a56ac53348
Pere Saurina <pere.saurina@gmail.com>
2016-11-30 13:11:47 +0100

    no message

--- sitemaps_xml/analitzar_doc.php
+++ sitemaps_xml/analitzar_doc.php
@@ -16,6 +16,8 @@ foreach ($xml->url as $url_list) {
     echo "<br>" . $i . " web : " . $url;
 
     if (Indexeds::existUrlDB($url) == '0') {
+            $igoogle++;    
+            if ($igoogle > 5) die();
             echo " - url no existeix en bbdd";
             $googleUrl = new GoogleUrl();
             $googleUrl->setLang('fr') // lang allows to adapt the query (tld, and google local params)
@@ -24,7 +26,7 @@ foreach ($xml->url as $url_list) {
             $simpsonPage1 = $googleUrl->setPage(0)->search($url); // simpsons results page 1 (results 1-20)            
             // GET NATURAL RESULTS
             $positions = $simpsonPage1->getPositions(); 
-            $igoogle++;    
+
             try{
                 //foreach ($positions as $result) {
                 //entrem aquí les N vegades del foreach....i només hi hem d'entrar un cop!
@@ -58,7 +60,7 @@ foreach ($xml->url as $url_list) {
             }catch (Exception $e){
                 echo "  -> Saltem URL";
             }  
-            if ($igoogle == 5) break();
+
             sleep(30);       
     }else{
         $i++;
